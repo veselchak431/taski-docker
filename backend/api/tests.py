5eb1c469fa812e194e615e2aecf5e389ca520e21
Катечкин Андрей <Krokik@mail.ru>
2023-09-19 18:54:40 +0300

    Add postgres tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -18,4 +18,4 @@ class TaskiAPITestCase(TestCase):
         data = {'title': 'Test', 'description': 'Test'}
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
-        self.assertTrue(models.Task.objects.filter(title='Test').exists()) 
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
